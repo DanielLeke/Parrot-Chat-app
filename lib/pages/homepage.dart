745e69b3446d5baf16bb4d36f0e9e378e69fadbb
Daniel Adeleke <danieladeleke05@gmail.com>
2024-12-16 20:23:15 +0100

    Added a home page and did some initial styling

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text(
+            'Parrot',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'DM Sans',
+              fontSize: 22
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
