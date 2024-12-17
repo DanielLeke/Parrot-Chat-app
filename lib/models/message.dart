e30269eb8ca2b7a70b0deec7fb7a19079489832e
Daniel Adeleke <danieladeleke05@gmail.com>
2024-12-17 22:18:15 +0100

    Removed unnecessary variables in the message class and working on the chat service

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,20 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Message {
-  late final String senderName;
   late final String senderId;
   late final String senderEmail;
-  late final String receiverName;
   late final String receiverId;
   late final String receiverEmail;
   late final String message;
   late final Timestamp timestamp;
 
   Message(
-      {required this.senderName,
+      {
       required this.senderId,
       required this.senderEmail,
-      required this.receiverName,
       required this.receiverId,
       required this.receiverEmail,
       required this.message,
@@ -22,10 +19,8 @@ class Message {
 
   Map<String, dynamic> toMap() {
     return {
-      'senderName': senderName,
       'senderId': senderId,
       'senderEmail': senderEmail,
-      'receiverName': receiverName,
       'receiverId': receiverId,
       'receiverEmail': receiverEmail,
       'message': message,
--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -4,4 +4,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 class ChatService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> sendMessage(String receiverId, String message) async {
+    final String currentUserId = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email.toString();
+    final Timestamp timestamp = Timestamp.now();
+  }
 }
