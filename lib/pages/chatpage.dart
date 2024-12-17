32bb6a2efa033ff67ffa1850198a4807f50f0031
Daniel Adeleke <danieladeleke05@gmail.com>
2024-12-17 19:52:56 +0100

    Created a chat page

---
+++ lib/pages/chatpage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Chatpage extends StatefulWidget {
+  final String receiverUserEmail;
+  final String receiverUserId;
+
+  const Chatpage(
+    {
+      super.key,
+      required this.receiverUserEmail,
+      required this.receiverUserId
+    }
+  );
+
+  @override
+  State<Chatpage> createState() => _ChatpageState();
+}
+
+class _ChatpageState extends State<Chatpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
