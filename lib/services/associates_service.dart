d91ceaaa02b886c4351b523d88be170e825a5d52
Daniel Adeleke <danieladeleke05@gmail.com>
2024-12-25 09:40:04 +0100

    Created a function in AssociatesService to create a collection of users in the firebase firestore because they have another user of the app as a contact on their phone

--- lib/services/associates_service.dart
+++ lib/services/associates_service.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:contacts_service/contacts_service.dart';
+import 'package:parrot/services/format_number_service.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class AssociatesService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FormatNumberService _formatNumberService = FormatNumberService();
 
   Future<Iterable<Contact>?> getContacts() async {
     Iterable<Contact>? contacts;
@@ -19,19 +21,28 @@ class AssociatesService {
     Iterable<Contact>? contacts = await getContacts();
     if (contacts != null) {
       for (Contact contact in contacts) {
-        QuerySnapshot querySnapshot = await _firestore
-            .collection('users')
-            .where('number', isEqualTo: contact.phones!.first.value)
-            .get();
-        if (querySnapshot.docs.isEmpty) {
-          // Associate not found
-        } else {
-          _firestore.collection('associates').doc().set({
-            'uid': querySnapshot.docs.first.id,
-            'email': querySnapshot.docs.first['email'],
-            'name': querySnapshot.docs.first['name'],
-            'number': querySnapshot.docs.first['number'],
-          });
+        if (contact.phones != null && contact.phones!.isNotEmpty) {
+          String? phoneNumber = contact.phones!.first.value;
+          if (phoneNumber != null) {
+            String formattedNumber =
+                await _formatNumberService.formatPhoneNumber(
+              number: phoneNumber,
+            );
+
+            QuerySnapshot querySnapshot = await _firestore
+                .collection('users')
+                .where('number', isEqualTo: formattedNumber)
+                .get();
+
+            if (querySnapshot.docs.isNotEmpty) {
+              _firestore.collection('associates').doc().set({
+                'uid': querySnapshot.docs.first.id,
+                'email': querySnapshot.docs.first['email'],
+                'name': querySnapshot.docs.first['name'],
+                'number': formattedNumber,
+              });
+            }
+          }
         }
       }
     }
